37bde156acfc54896aae9f256355956f19080224
Sebastian Utbult <kreuzweg@gmail.com>
2025-09-04 00:20:43 +0200

    Fix broken bootstrapper

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 // start bootstrapping
+require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
-require __DIR__ . '/../vendor/autoload.php';
 session_start();
-$app = new \LOD\App();
-$loader = new \Twig\Loader\FilesystemLoader('../views');
+$app = \LOD\App::getInstance();
+$loader = new \Twig\Loader\FilesystemLoader('./views');
 $twig = new \Twig\Environment($loader, []);
 
 // Routing
